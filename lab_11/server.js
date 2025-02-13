ac52b5f44199ebbbe14324b7b07295b27c99d8bd
Favour_U <favouruduebholo13@gmail.com>
2025-02-13 11:42:52 +0000

    Importing knock knock jokes

--- lab_11/server.js
+++ lab_11/server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var knockknock = require('knock-knock-jokes');
+
 
 app.get('/', function(req, res){
     res.send("Hello world! by express");
@@ -10,6 +12,12 @@ app.get('/', function(req, res){
 app.get("/test", function(req, res){
     res.send("Hello this is route 2");
     
+});
+
+app.get('/joke', function (req, res){
+    var randomJoke = knockknock()
+    res.send(randomJoke);
+
 });
 
 app.listen(8080);
