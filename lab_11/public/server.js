ab3c31b7a6924d6e77fb484aedb9572a8ac2cf71
Favour_U <favouruduebholo13@gmail.com>
2025-02-13 20:50:48 +0000

    sending files to server for processig

--- lab_11/public/server.js
+++ lab_11/public/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+app.use(express.static('public'))
 
 
 app.get('/', function(req, res){
@@ -63,6 +64,13 @@ app.get('/calc', function(req,res){
 
     }
     
+});
+
+app.get('/getform', function(req,res){
+    var name = req.query.name;
+    var quest = req.query.quest;
+    res.send("Hi "+name+" I am sure you will "+quest) ;
+
 });
 
 
