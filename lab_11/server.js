87620996400a59347c95da1d04b7d1866c08d852
Favour_U <favouruduebholo13@gmail.com>
2025-02-13 12:06:14 +0000

    add route

--- lab_11/server.js
+++ lab_11/server.js
@@ -13,6 +13,13 @@ app.get("/test", function(req, res){
     
 });
 
+app.get('/add' , function(req,res){
+    var x = req.query.x;
+    var y = req.query.y;
+    res.send("X + Y =" + (x+y));
+
+
+});
 
 
 app.listen(8080);
