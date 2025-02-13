34fe557b81f33cd70d1d278be69edf58df7ef3db
Favour_U <favouruduebholo13@gmail.com>
2025-02-13 12:25:38 +0000

    just some testing

--- lab_11/server.js
+++ lab_11/server.js
@@ -14,8 +14,8 @@ app.get("/test", function(req, res){
 });
 
 app.get('/add' , function(req,res){
-    var x = parseInt(req.query.x, 10);
-    var y = parseInt(req.query.y, 10);
+    var x = parseInt(req.query.x);
+    var y = parseInt(req.query.y);
 
     if(isNaN(x) ||isNaN(y)){
     res.send("Invalid input. Please provide numbers for x and y.");
