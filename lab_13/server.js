36f8dc7891ff0635a181a2d5a3535b9b46981642
Favour_U <favouruduebholo13@gmail.com>
2025-02-25 16:11:00 +0000

    changes

--- lab_13/server.js
+++ lab_13/server.js
@@ -1,26 +1,21 @@
 const MongoClient = require('mongodb-legacy').MongoClient;
 const url = 'mongodb://127.0.0.1:27017';
-const client = new MongoClient(ur);
-const dbname ='star_wars_quotes';
-
-//code to link to the express mosule
+const client = new MongoClient(url);
+const dbname = 'star_wars_quotes';
+//code to link to the express module
 const express = require('express');
 const app = express();
-
-//code to define the public folder
-app.use(express.static('public'));
-
+//code to define the public
+app.use(express.static('public'))
 var db;
-
-//run the connect method
+//run the connect method.
 connectDB();
-
-async function connectDB(){
- //Use connect method to connect to the server
+async function connectDB() {
+ // Use connect method to connect to the server
  await client.connect();
- console.log('Connected suffesfully to the server');
+ console.log('Connected successfully to server');
  db = client.db(dbname);
- //Everything is good lets start
+
 
  app.get('/all', function(req, res) {
     db.collection('quotes').find().toArray(function(err, result) {
@@ -35,5 +30,8 @@ async function connectDB(){
     res.send(output);
     });
    });
+   
+ //everything is good lets start
  app.listen(8080);
 }
+
